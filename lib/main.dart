a5f554ef138bc326aebc1931a6b8362c9976e19d
Tiyas Aria Pratiwi <71452274+TiyasAria@users.noreply.github.com>
2022-09-09 11:43:00 +0700

    add splashscreen and onBoard

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,30 @@
 import 'package:auth_account/pages/login.dart';
+import 'package:auth_account/pages/onBoard.dart';
+import 'package:auth_account/pages/splashscreen.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() => runApp(const MainApp());
+void main()  async{
+  WidgetsFlutterBinding.ensureInitialized() ;
+  await getIntroduction() ;
+  runApp(const MainApp());
+}
+
+// this code for sharedPreferences
+ int introduction = 0 ;
+ Future getIntroduction()  async{
+  final prefs =  await SharedPreferences.getInstance() ;
+
+  int? intro = prefs.getInt('introduction') ;
+  print('introduction : $intro') ;
+  if (intro != null && intro == 1){
+    return introduction = 1; 
+  }
+  
+  prefs.setInt('introduction', 1) ;
+
+ }
+
 
 class MainApp extends StatefulWidget {
   const MainApp({Key? key}) : super(key: key);
@@ -13,9 +36,15 @@ class MainApp extends StatefulWidget {
 class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FormLogin(),
+      //
+      initialRoute: SplashScreen.routeName,
+      routes: {
+        SplashScreen.routeName : (context) => SplashScreen() ,
+        OnBoardingPage.routeName : (context) => OnBoardingPage(),
+        FormLogin.routeName : (context) => FormLogin()
+      },
     );
   }
 }
---
+++ lib/pages/splashscreen.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+
+import 'package:auth_account/main.dart';
+import 'package:auth_account/pages/login.dart';
+import 'package:auth_account/pages/onBoard.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  static const routeName = '/splashScreen' ;
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    splashScreen() ;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffF2F7FA),
+      body: Center(
+        child: Image.asset('assets/blanjaloka.png', width: 300,),
+      ),
+    );
+  }
+
+  splashScreen() async {
+    var duration =  const Duration(seconds: 4) ;
+    return Timer(duration, (){
+      if (introduction == 0){
+        Navigator.pushReplacementNamed(context, OnBoardingPage.routeName) ;
+      } else {
+        Navigator.pushReplacementNamed(context, FormLogin.routeName) ;
+
+      }
+    }) ;
+  }
+
+}
